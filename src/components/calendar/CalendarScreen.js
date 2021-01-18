219b661e7cd728086fe5949052914a5140d647a9
eacz <estebanacanteros@gmail.com>
2021-01-18 20:28:26 -0300

    react big calendar added

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../ui/Navbar';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
 
 const CalendarScreen = () => {
-    return (
-        <div>
-            <h1>Calendar Screen</h1>
-        </div>
-    )
-}
+    const events = [
+        {
+            title: 'test title',
+            start: moment().toDate(),
+            end: moment().add(2, 'hours').toDate(),
+            bgcolor: '#fafafa',
+        },
+    ];
 
-export default CalendarScreen
+    return (
+        <div className="calendar-screen">
+            <Navbar />
+
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+            />
+        </div>
+    );
+};
+
+export default CalendarScreen;
